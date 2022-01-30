ac8eda94ea7e99c3f1592ee1fca731b01757fbc3
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-01-30 19:35:07 -0300

    CRUD Genérico completo

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -3,14 +3,14 @@ using Dapper.Contrib.Extensions;
 
 namespace Blog.Models
 {
-    [Table("[Tag")]
+    [Table("[Tag]")]
     public class Tag
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
         
-        // [Write(false)]
-        // public List<Post> Posts { get; set; }
+        [Write(false)]
+        public List<Post> Posts { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Blog.Models;
 using Blog.Repositories;
-using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
 namespace Blog
@@ -14,31 +13,101 @@ namespace Blog
         {
             SqlConnection connection = new SqlConnection(CONNECTION_STRING);
             connection.Open();
-            ReadUsers(connection);
-            ReadRoles(connection);
+            // ReadUsers(connection);
+            // ReadRoles(connection);
+            // ReadTags(connection);
+            // ReadUser(connection);
+            // CreateUser(connection);
+            // UpdateUser(connection);
+            // DeleteUser(connection);
+            // ReadUsers(connection);
+            // DeleteUserById(connection);
 
+            ReadUsersWithRoles(connection);                        
             connection.Close();
         }
 
-        public static void ReadUsers(SqlConnection connection)
+        public static void ReadUsersWithRoles(SqlConnection connection)
         {
             var repository = new UserRepository(connection);
-            var users = repository.GetAll();
-            
+            var items = repository.GetWithRoles();
 
-            foreach (var user in users)
+            foreach (var item in items)
             {
-                Console.WriteLine(user.Name);
+                Console.WriteLine(item.Name);
+                foreach (var role in item.Roles)
+                {
+                    Console.WriteLine($" - {role.Name}");
+                }
             }
         }
 
+        public static void ReadUser(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var item = repository.Get(3);
+            Console.WriteLine(item.Name);
+        }
+
+        public static void CreateUser(SqlConnection connection)
+        {
+            User user = new User()
+            {
+                Name = "Eduardo",
+                Email = "eduardo@gmail.com",
+                PasswordHash = "123",
+                Bio = "Dev DotNet",
+                Image = "https://",
+                Slug = "eduardo"
+            };
+
+            var repository = new Repository<User>(connection);
+            repository.Create(user);
+            Console.WriteLine("Usuário cadastrado com sucesso!");
+        }
+
+        public static void UpdateUser(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var user = repository.Get(4);
+            user.Email = "eduardo@outlook.com";
+            repository.Update(user);
+            Console.WriteLine("Usuário atualizado com sucesso!");
+        }
+
+        public static void DeleteUser(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var user = repository.Get(4);
+            repository.Delete(user);
+            Console.WriteLine("Usuário excluído com sucesso!");
+        }
+
+        public static void DeleteUserById(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            repository.Delete(3);
+            Console.WriteLine("Usuário excluído com sucesso!");
+        }
+
+
         public static void ReadRoles(SqlConnection connection)
         {
-            var repository = new RoleRepository(connection);
-            var roles = repository.GetAll();
-            foreach (var role in roles)
+            var repository = new Repository<Role>(connection);
+            var items = repository.GetAll();
+            foreach (var item in items)
             {
-                Console.WriteLine(role.Name);
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.GetAll();
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Name);
             }
         }
 
